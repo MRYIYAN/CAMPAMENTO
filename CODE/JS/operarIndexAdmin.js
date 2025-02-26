a46e0f19da98c2a7c8598590ee871b67bb4581ef
JingxiangWang_ExamenGit <jingxiangwang2005@gmail.com>
2025-02-26 10:42:36 +0100

    Actualizacion de IndexAdmin

--- CODE/JS/operarIndexAdmin.js
+++ CODE/JS/operarIndexAdmin.js
@@ -146,6 +146,17 @@ function cerrarSesionSeguro() {
       }
     });
 }
+
+
+//menu hamburguesa
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const navContainer = document.getElementById("nav-container");
+
+  menuIcon.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+  });
+});
 //-----------------------------------------------------------------------------------------------------------//
 //                                           FIN DE JS DE NAVBAR
 //-----------------------------------------------------------------------------------------------------------//
